507398c30a1433b01d92b0c71bf64f57b04886bb
ManjuGGitHub <139679447+ManjuGGitHub@users.noreply.github.com>
2024-10-16 23:38:35 +0530

    Update explorer.sh by manju

--- Challenges/Day_2/explorer.sh
+++ Challenges/Day_2/explorer.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
-# Part 1: File and Directory Exploration
 echo "Welcome to the Interactive File and Directory Explorer!"
 
-while true; do
-    # List all files and directories in the current path
-    echo "Files and Directories in the Current Path:"
-    ls -lh
+#Listing all files and directories to all_files file
 
-    # Part 2: Character Counting
-    read -p "Enter a line of text (Press Enter without text to exit): " input
+ls $pwd >all_files
+#Create Array and add all the files and directory to that array
+readarray -t files <all_files
 
-    # Exit if the user enters an empty string
-    if [ -z "$input" ]; then
-        echo "Exiting the Interactive Explorer. Goodbye!"
-        break
-    fi
+#Print file name and its size in Human Readable format
+for file in ${files[@]};do
+     du -h $file | awk '{print $2,"(",$1,")"}'
+done
 
-    # Calculate and print the character count for the input line
-    char_count=$(echo -n "$input" | wc -m)
-    echo "Character Count: $char_count"
+#$User interactive script to get the input and print word count, We can use wc also to count words
+while true;do
+
+        read -p "Enter a line of text (Press Enter without text to exit): " user_input
+        echo ""
+        if [[ -z "$user_input" ]];then
+                echo "Exiting the Interactive Explorer. Goodbye!"
+                break
+        else
+                echo "Character Count: ${#user_input}"
+                echo ""
+        fi
+
 done
