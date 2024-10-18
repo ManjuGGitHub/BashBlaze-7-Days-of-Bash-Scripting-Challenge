d69cbe50d9639491d94400f957d3e5d1e87bceba
ManjuGGitHub <139679447+ManjuGGitHub@users.noreply.github.com>
2024-10-19 00:01:28 +0530

    Update Sol_monitor_process.sh by manju

--- Challenges/Day_4/Sol_monitor_process.sh
+++ Challenges/Day_4/Sol_monitor_process.sh
@@ -1,48 +1,43 @@
 #!/bin/bash
 
-# Function to check if the specified process is running
-is_process_running() {
-    if pgrep -x "$1" >/dev/null; then
-        return 0
-    else
-        return 1
-    fi
+#function to check given process is running or not
+function is_processRunning(){
+        if pgrep -x "$1";then
+                return 0
+        else
+                return 1
+        fi
 }
 
-# Function to restart the process using systemctl
-restart_process() {
-    local process_name="$1"
-    echo "Process $process_name is not running. Attempting to restart..."
+#Function to restart the process
+function restartProcess(){
+        local process="$1"
+        echo "$process is not Running. Attempting to start $process process"
 
-    # Check if the user has the privilege to restart the process
-    if sudo systemctl restart "$process_name"; then
-        echo "Process $process_name restarted successfully."
-    else
-        echo "Failed to restart $process_name. Please check the process manually."
-    fi
+        #starting process
+        if sudo systemctl start "$process";then
+                echo "$process Process starting now"
+        else
+                echo "$process Failed to start... reattempting again."
+        fi
+
 }
 
-# Check if a process name is provided as an argument
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <process_name>"
-    exit 1
-fi
-
-process_name="$1"
-max_attempts=3
+maximumAttempt=3
 attempt=1
 
-# Loop to check and restart the process
-while [ $attempt -le $max_attempts ]; do
-    if is_process_running "$process_name"; then
-        echo "Process $process_name is running."
-    else
-        restart_process "$process_name"
-    fi
+#Maximum 3 attempts to it will check, If process is running then it will exit else it will try for another time and try to restrat process
+while [ $attempt -lt $maximumAttempt ];do
+        if is_processRunning "$1";then
+                echo "$1 Process is running."
+                exit
+        else
+                restartProcess "$1"
+        fi
 
-    attempt=$((attempt + 1))
-    sleep 5  # Wait for 5 seconds before the next check
+        attempt=$((attempt + 1))
+
+        sleep 5
 done
-
-echo "Maximum restart attempts reached. Please check the process manually."
-
+#If the process failed to restart in maximum 3 attempts then it will print user message to check manually.
+echo "Maximum restart attempts failed, please check process manually"
